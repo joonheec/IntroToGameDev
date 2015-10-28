d564ed5dc8915e8b9f8ca14f0a98982cb92e7625
Joonhee Choi <joonheechoi@172-16-16-178.DYNAPOOL.NYU.EDU>
2015-10-28 18:19:57 -0400

    Created Office Entrance

---
+++ HW Week 9 Text Adventure/Assets/Scenes/TextAdventureScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextAdventureScript : MonoBehaviour {
+	
+	public string currentRoom = "Office Entrance";
+	public string room_north = "";
+	public string room_south = "";
+	public string room_west = "";
+	public string room_east = "";
+
+
+		
+		
+		
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string textBuffer = "";
+		room_east = "";
+		room_north = "";
+		room_south = "";
+		room_west = "";
+		switch (currentRoom) {
+		case "Office Entrance":
+			textBuffer = "You have entered the office building. Find and kill your annoying boss!\n"+
+				"Find the key to the Storage Room\n"+"The Main Room is to the north";
+			room_north = "Storage Room";
+
+
+			break;
+		default:
+			break;
+
+
+		}
+	}
+}
