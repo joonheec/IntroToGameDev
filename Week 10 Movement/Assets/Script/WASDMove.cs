e9213e95fdfdd86419d9a23b5e1fdfd6ac7dd018
Joonhee Choi <joonheechoi@172-16-22-249.DYNAPOOL.NYU.EDU>
2015-11-02 17:18:05 -0500

    Basic 8 direction movement

---
+++ Week 10 Movement/Assets/Script/WASDMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WASDMove : MonoBehaviour {
+	public Vector3 fwdMove;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check if player is pressing w
+		if (Input.GetKey(KeyCode.W)) {
+			//move character forward
+			transform.position += transform.forward * Time.deltaTime * moveSpeed; 
+
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+
+
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position -= transform.right * Time.deltaTime * moveSpeed;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += transform.right * Time.deltaTime * moveSpeed;
+		}
+	}
+}
