0a38f9658bf8d0c1c3eb728ba46e59a7251e50c6
Joonhee Choi <joonheechoi@172-16-22-249.DYNAPOOL.NYU.EDU>
2015-11-02 18:12:05 -0500

    Top Down Click to Move.

---
+++ Week 10 Movement/Assets/Script/ClickToMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// check if player clicks
+	if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			// get location of click
+			//transform.position = Input.mousePosition;
+			if(Physics.Raycast(ray, out hit) && hit.collider.tag == "Ground") {
+				Vector3 move;
+				//get directions to destination
+				//vector between destination and current location
+				move = hit.point - transform.position;
+				// set that vector's magnitude to 1
+				move.Normalize();
+				transform.position += move * Time.deltaTime * moveSpeed;
+				// get location of click
+				// LERP IS OK, but i want more control!
+				//transform.position = Vector3.Lerp(transform.position, hit.point, 0.2f);
+			}
+		}
+
+	// move player toward click location
+	}
+ }
--- Week 10 Movement/Assets/Script/MouseLook.cs
+++ Week 10 Movement/Assets/Script/MouseLook.cs
@@ -12,12 +12,12 @@ public class MouseLook : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// check if player drags mouse
-		if (Input.GetAxis ("Mouse X") != 0) {
+		if (Input.GetAxis ("Mouse X") > 1) {
 			//rotate player in direction of mouse
 			transform.Rotate(transform.up, Input.GetAxis("Mouse X") * lookSensitivity);
 		
 		}
-		if (Input.GetAxis ("Mouse Y") != 0) {
+		if (Input.GetAxis ("Mouse Y") > 1) {
 			//rotate player in direction of mouse
 			transform.Rotate (transform.right, Input.GetAxis ("Mouse Y") * ySensitivity);
 		}
